f9d9b204342dcae26700a2115630141a387ebfb0
Your Name <you@example.com>
2025-12-10 16:49:38 +0900

    '123'

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -21,6 +21,7 @@ export {
 
 SplashScreen.preventAutoHideAsync();
 
+
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
